804db3560135032ca6060bb34441f9e8110dfffb
Abdulloh <n.solahiddin@gmail.com>
2025-12-12 21:06:36 -0600

    day one inshaAllah

---
+++ word_guesser.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * NO chatgpt or any other AI use google only,  
+ * GO TO: https://addons.mozilla.org/en-US/firefox/addon/hide-google-ai-overviews/
+ * enable it when you are searching on the internet while programming. You can disable later if you want.
+ */
+
+
+
+
+ /**
+  * Task 1:
+  * store a char variable and make the user guess it.
+  */
+
+
+  int main(){
+
+    // store a variable
+
+    // take an input from the user
+
+    // check if the input matches what you have hidden
+
+    // if it matches, say it matches and end the program
+
+    // if it does not match, say it does not and end the program
+
+    return 0;
+  }

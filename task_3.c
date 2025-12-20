bbad769084eeb3db24a28a1887dd262dcc55d0ef
Abdulloh <n.solahiddin@gmail.com>
2025-12-19 22:52:52 -0600

    added the 3rd task

---
+++ task_3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * NO chatgpt or any other AI use google only,  
+ * GO TO: https://addons.mozilla.org/en-US/firefox/addon/hide-google-ai-overviews/
+ * enable it when you are searching on the internet while programming. You can disable later if you want.
+ */
+
+
+
+
+ /**
+  * Task 1:
+  * store a char variable and make the user guess it.
+  */
+
+ /**
+  * Task 2:
+  * make the user guess it in a while loop until they enter the correct answer, 
+  * and keep track of the times the user guessed before they guessed it correctly.
+  */
+
+/** 
+ * @date 12/20/2025
+ * Task 3:
+ * ask the user if they want to play the game again,
+ * 
+ * if YES: start the game again with the same hidden letter
+ * if NO: end the game.
+ */
+
+
+  int main(){
+    // add the new functionality to your previous code that you have, and send it to me.
+
+
+    return 0;
+  }
